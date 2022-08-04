4a8824356367280f55fd13e5731725921bca7714
mugiwarathesunny <109825719+mugiwarathesunny@users.noreply.github.com>
2022-08-04 11:06:10 +0100

    Updated

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = require('./app')
 
 require('dotenv').config()
 
-const port = process.env.PORT || PORT
+const port = process.env.PORT || 2004
 
 app.listen(port, () => console.log ('Welcome to port ' + port))
